538c6f83d9ebc51afb0624005e36d69c8a6806da
Chinmay Kini <chinmaykini@gmail.com>
2015-01-24 15:38:44 -0800

    Initial Commit

---
+++ RottenTomatoes/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RottenTomatoes
+//
+//  Created by Chinmay Kini on 1/24/15.
+//  Copyright (c) 2015 Chinmay Kini. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ RottenTomatoes/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  RottenTomatoes
+//
+//  Created by Chinmay Kini on 1/24/15.
+//  Copyright (c) 2015 Chinmay Kini. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
